127e541914d0b400415ea8b6e7dbd12bdd87edb5
ranafge <ranafge@gmail.com>
2024-01-15 18:32:30 +0600

    tokio stream started

---
+++ examples/streams_iteration.rs
@@ -0,0 +1,16 @@
+// a stream is an asynchronoush series of values. equivalent to std::iter::Iterator
+//Iteration
+
+// rust programming language does not support for loop for asnchronouch that replace with while let 
+
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+
+async fn main() {
+    let mut stream = tokio_stream::iter(&[1,2,3]);
+    while let Some(v) = stream.next().await {
+        // next() return Option<t> t is the steam value type
+        println!("GOT = {:?}", v);
+    }
+}
